cadf9df0130d2773bd6038617efab6a3675c9348
Nativemobile-labs <ptradeepyaduvanshi2802@gmail.ocm>
2025-03-12 01:01:53 +0530

    mona brnach

---
+++ screens/mona.js
@@ -0,0 +1,44 @@
+commit 0f20ba7c61c3b15aedb6cbc9f7455fc74393b82a (origin/devlop)
+Author: Nativemobile-labs <ptradeepyaduvanshi2802@gmail.ocm>
+Date:   Wed Mar 12 00:31:13 2025 +0530
+
+    initial commit
+
+commit d06889ca827937d904ba5fc9172e10279783a67e (main)
+Author: Pradeep  Kumar <155940616+Nativemobile-labs@users.noreply.github.com>
+Date:   Wed Mar 12 00:23:12 2025 +0530
+
+    Initial commit
+bhardwaj@Bhardwajs-MacBook-Air pratices % git push origin --delete pradeep
+To https://github.com/Nativemobile-labs/pratices.git
+ - [deleted]         pradeep
+bhardwaj@Bhardwajs-MacBook-Air pratices % git fetch origin main
+From https://github.com/Nativemobile-labs/pratices
+ * branch            main       -> FETCH_HEAD
+bhardwaj@Bhardwajs-MacBook-Air pratices % git pull
+There is no tracking information for the current branch.
+Please specify which branch you want to merge with.
+See git-pull(1) for details.
+
+    git pull <remote> <branch>
+
+If you wish to set tracking information for this branch you can do so with:
+
+    git branch --set-upstream-to=origin/<branch> devlop
+
+bhardwaj@Bhardwajs-MacBook-Air pratices % git checkout -b mona
+Switched to a new branch 'mona'
+bhardwaj@Bhardwajs-MacBook-Air pratices % git pull main
+fatal: 'main' does not appear to be a git repository
+fatal: Could not read from remote repository.
+
+Please make sure you have the correct access rights
+and the repository exists.
+bhardwaj@Bhardwajs-MacBook-Air pratices % git branch
+  devlop
+  main
+* mona
+  pradeep
+bhardwaj@Bhardwajs-MacBook-Air pratices % git branch -d pradeep
+Deleted branch pradeep (was a7b36b3).
+bhardwaj@Bhardwajs-MacBook-Air pratices % git branch
