ee81f3efd588cb3e2db53c6dd2e70bf243cf433f
Nativemobile-labs <ptradeepyaduvanshi2802@gmail.ocm>
2025-03-12 02:39:32 +0530

    Revert "new changes"
    This reverts commit aaeebe90ef123585d5ea45b2e288260169d54805.

--- screens/sonam.js
+++ screens/sonam.js
@@ -5,12 +5,3 @@ dbadghaghdg
 basdgahjgdhja
 bsadhagdhj
 hgfhgahsjgfhj
-
-
-
-bbbbbbbbbbb
-nnnnnnnn
-,,,,,,,,
-...AbortController.apply.apply.apply.apply.apply.apply.apply.apply.apply.apply.apply.apply.apply.apply
-.//////.....lll;;;;;;pk;lk;lkl;k;g
-gfhgfjhgsfhjgsjhdgf
